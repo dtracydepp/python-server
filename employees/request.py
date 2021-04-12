a0de7d9fc36e0116e9035cbbfe6f968220121430
dtracydepp <tracydepp82@gmail.com>
2021-04-12 16:29:41 -0500

    updated comments

--- employees/request.py
+++ employees/request.py
@@ -25,10 +25,10 @@ def get_all_employees():
 
     # Function with a single parameter
 def get_single_employee(id):
-    # Variable to hold the found animal, if it exists
+    # Variable to hold the found employee, if it exists
     requested_employee = None
 
-    # Iterate the ANIMALS list above. Very similar to the
+    # Iterate the EMPLOYEES list above. Very similar to the
     # for..of loops you used in JavaScript.
     for employee in EMPLOYEES:
         # Dictionaries in Python use [] notation to find a key
--- locations/request.py
+++ locations/request.py
@@ -18,10 +18,10 @@ def get_all_locations():
 
     # Function with a single parameter
 def get_single_location(id):
-    # Variable to hold the found animal, if it exists
+    # Variable to hold the found location, if it exists
     requested_location = None
 
-    # Iterate the ANIMALS list above. Very similar to the
+    # Iterate the LOCATIONS list above. Very similar to the
     # for..of loops you used in JavaScript.
     for location in LOCATIONS:
         # Dictionaries in Python use [] notation to find a key
