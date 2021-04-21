2e653801e45240bf71dedd1221187806732a12a7
dtracydepp <tracydepp82@gmail.com>
2021-04-21 11:15:07 -0500

    updated do get with key and resource, get employees by location function to query for employees by location

--- employees/request.py
+++ employees/request.py
@@ -156,3 +156,30 @@ def get_single_employee(id):
                             )
 
         return json.dumps(employee.__dict__)
+
+def get_employees_by_location(location_id):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id      
+        FROM employee e
+        WHERE e.location_id = ?
+        """, (location_id, ))
+
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['address'],
+                             row['location_id'])
+                            
+            employees.append(employee.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(employees)
